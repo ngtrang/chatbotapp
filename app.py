3ad990f46b2f118ec69bede9b11479da71500be9
Mai Trang <m.trang91@gmail.com>
2018-12-09 22:34:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,94 @@
-from rasa_core.channels import HttpInputChannel
-from rasa_core.channels.facebook import FacebookInput
-from rasa_core.agent import Agent
-from rasa_core.interpreter import RasaNLUInterpreter
-import yaml
 import os
-#from rasa_core.utils import EndpointConfig
-from rasa_core.interpreter import RegexInterpreter
+import sys
+import json
+import chatbot_rus
+from datetime import datetime
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+                    #answer = chatbot_rus.chattoBot(message_text)
+                    from rasa_core.channels import HttpInputChannel
+                    from rasa_core.channels.facebook import FacebookInput
+                    from rasa_core.agent import Agent
+                    from rasa_core.interpreter import RasaNLUInterpreter
+                    import yaml
+                    interpreter = RasaNLUInterpreter("models/nlu/default/current")
+                    agent = Agent.load("models/dialogue", interpreter=interpreter) 
+                    send_message(sender_id, agent.handle_channel(message_text))
+
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = unicode(msg).format(*args, **kwargs)
+        print (u"{}: {}".format(datetime.now(), msg))
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
+
 
 if __name__ == '__main__':
-   interpreter = RasaNLUInterpreter("models/nlu/default/current")
-   agent = Agent.load("models\\dialogue", interpreter= interpreter) # RegexInterpreter())
-
-   input_channel = FacebookInput(
-      fb_verify= os.environ["VERIFY_TOKEN"],
-      fb_secret=os.environ["FB_SECRET"],
-      fb_access_token=os.environ["PAGE_ACCESS_TOKEN"],
-      )
-
-   # or `agent.handle_channel(...)` for synchronous handling
-   agent.handle_channel(HttpInputChannel( "/app", input_channel))
-
+    app.run(debug=True)
